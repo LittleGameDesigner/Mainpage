9439705d0362531c78ef82f68d754ce333cdd17c
williamwu111 <xiaoyang.wu@mail.utoronto.ca>
2020-02-09 15:49:49 -0500

    logout

--- app/src/main/java/com/example/main_page/Settings.java
+++ app/src/main/java/com/example/main_page/Settings.java
@@ -1,5 +1,6 @@
 package com.example.main_page;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,24 +10,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        button = findViewById(R.id.Change_Password);
+        button.setOnClickListener(new View.OnClickListener(){
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v){
+                clickbutton();
             }
         });
+
+    }
+
+    public void clickbutton() {
+        Intent intent = new Intent(this, Logout.class);
+        startActivity(intent);
     }
 
 }
